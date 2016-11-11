e742f8c173d32a0e35b495a41b256806f25ef712
Jonathon Israel <jisrael11@live.com>
2016-11-11 13:04:44 -0600

    update index.php

---
+++ Website_Files/index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php
+        include_once("navbar.php");
+        ?>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </body>
+</html>
