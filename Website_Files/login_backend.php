52a3e88e9c5335020a6e7330835f9286b87af83f
raj8r8 <raj8r8@mail.missouri.edu>
2016-11-18 11:15:36 -0600

    Update and rename login_php.php to login_backend.php

--- Website_Files/login_backend.php
+++ Website_Files/login_backend.php
@@ -2,7 +2,7 @@
 	if(isset($_SESSION['authentication'])){?>
 		<script type="text/javascript">
         <!--
-        window.location = "index.php"
+        window.location = "login2.php"
         //-->
         </script>
         You're already logged in, redirecting you.
@@ -39,11 +39,3 @@
 			}else{}
 ?>
 
-<div class='login'>
-            <form action='login_php.php' method='post'>
-               <br/> You're not logged in as an admin, please do so. <br/><br/>
-                <?php if($wrongflag == 1){ echo "<font size='2px' color='red' face='Arial'> Wrong Username/Password </font><br/>";} ?>
-                <input class='login-text' type='text' name='username' value='Username' onFocus="if(this.value == 'Username') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Username';}">
-                <input class='login-text' type='password' name='password' value='Password' onFocus="if(this.value == 'Password') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Password';}">
-                <input class='login-button' type='submit' name='login' value='Login'>
-            </form>
