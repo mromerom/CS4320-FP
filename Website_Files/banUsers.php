95e61739c5f434a429639e006978724d1801acaa
Kevin Free <kcfk28@mail.missouri.edu>
2016-12-05 21:32:06 -0600

    Ban users

--- Website_Files/banUsers.php
+++ Website_Files/banUsers.php
@@ -4,6 +4,11 @@
         header("Location: login.php");
         exit();
     }
+
+    if($_SESSION['usertype'] != 'admin') {
+        header("Location: index.php");
+        exit();
+    }
 ?>
 <html>
     <head>
@@ -18,6 +23,75 @@
         <?php
         include_once("navbar.php");
         ?>
-        <p>Accessable only to admins, allows banning of users.</p>
+        <?php
+        switch ($_SESSION["message"])//Checks for flags
+        {
+          case 'unfound':
+            ?>
+            <div class="alert alert-warning">No user exists with this username.</div>
+            <?php
+          break;
+          case 'success':
+            ?>
+            <div class="alert alert-success">Successfully banned user.</div>
+            <?php
+          break;
+          case 'failed':
+            ?>
+            <div class="alert alert-danger">Ban user failed.</div>
+            <?php
+          break;
+          case 'admin':
+            ?>
+            <div class="alert alert-warning">You can't delete an admin user.</div>
+            <?php
+          break;
+          default:
+            break;
+        }
+        unset($_SESSION["message"]);
+        ?>
+
+        <div class="container">
+            <br>
+            <div class="row align-center">
+                <h1>Ban Users</h1>
+                <form action='banUsers.php' method='POST'>
+                    <input type="text" name="search">
+                    <input class="btn btn-info" type="submit" name="submit" value="Search">
+                </form>
+                <?php
+                if (isset($_POST["submit"])){
+                    $connection = new MongoClient();
+                    $db = $connection->collections;
+                    $collection = $db->users;
+
+                    $foundUser = $collection->findOne(array("username" => $_POST['search']));
+
+                    if($foundUser == NULL) {
+                        $_SESSION['message'] = 'unfound';
+                        header("Location: banUsers.php");
+                        exit();
+                    }
+
+                    if($foundUser['usertype'] == "admin") {
+                        $_SESSION['message'] = 'admin';
+                        header("Location: banUsers.php");
+                        exit();
+                    }
+
+                    if($collection->remove(array("username" => $_POST['search'])) == TRUE) {
+                        $_SESSION['message'] = "success";
+                        header("Location: banUsers.php");
+                        exit();
+                    } else {
+                        $_SESSION['message'] = "failed";
+                        header("Location: banUsers.php");
+                        exit();
+                    }
+                }
+                ?>
+            </div>
+        </div>
     </body>
 </html>
