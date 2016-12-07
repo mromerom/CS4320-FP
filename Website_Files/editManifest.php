3c0dc6874684dd22cc820801186483124e0c8b3d
RFTray <rft5g9@mail.missouri.edu>
2016-12-07 16:28:06 -0600

    Auto size text box

---
+++ Website_Files/editManifest.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header("Location: login.php");
+        exit();
+    }
+    if(!isset($_POST['datasetURL'])) {
+      $_SESSION["message"] = '-1';
+    } else {
+      //Connect to database and select manifests collection
+      $m = new MongoClient();
+      $db = $m->collections;
+      $collection = $db->manifests;
+
+      $foundManifest = $collection->findOne(array("datasetURL" => $_POST['datasetURL']));
+      $manifestString = json_encode($foundManifest, JSON_PRETTY_PRINT);
+    }
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script>
+            function auto_grow(element) {
+                element.style.height = "5px";
+                element.style.height = (element.scrollHeight+20)+"px";
+            }
+        </script>
+        <style>
+            textarea {
+                resize: none;
+                overflow: hidden;
+                min-height: 50px;
+                width: 330px;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        include_once("navbar.php");
+        ?>
+
+        <?php
+              switch ($_SESSION["message"])
+              {
+                case '-1':
+                  ?>
+                  <div class="alert alert-danger">Failed.</div>
+                  <?php
+                break;
+                default:
+                  break;
+              }
+              unset($_SESSION["message"]);
+        ?>
+        <h1>Edit Manifest</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="POST" class="col-md-4 col-md-offset-5">
+            <div class="input-group">
+                <div class="form-group">
+                    <textarea autofocus onfocus="auto_grow(this)" name="textarea"><?=$manifestString?></textarea>
+                </div>
+            </div>
+            <input class="btn btn-info" type="submit" name="submit" value="Submit">
+        </form>
+
+        <?php
+          if(isset($_POST['submit'])) {
+            //Connect to database and select manifests collection
+            $m = new MongoClient();
+            $db = $m->collections;
+            $collection = $db->manifests;
+
+          }
+          ?>
+    </body>
+</html>
